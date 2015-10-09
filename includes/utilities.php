b1372e311b40c65b205a2342b5b6b373bdac0a7e
Mark Nightingale <hello@marknightingale.net>
2015-10-09 11:27:57 +0200

    Refactored mn_language_selector and optimised code

--- includes/utilities.php
+++ includes/utilities.php
@@ -51,58 +51,80 @@ function get_country_flag( $post_id = '' ) {
 
 
 // Get language selector with just language code
-function mn_language_selector( $classes = ''){
-    $languages = icl_get_languages('skip_missing=0&orderby=code');
+/* $args : array or string (of classes - depreciated).
+    $args = array(
+        'classes' => string of classes,
+        'has_dividers' => bool (default false),
+        'format' => (inline-menu (default) | dropdown-menu | dropdown-button ),
+        'full_name' => bool (default false),
+        'order' => 'ASC',
+        'orderby' => 'code'
+    )
+    
+*/
+
+function mn_language_selector( $args = array() ){
+        $defaults = array(
+            'classes' => '',
+            'has_dividers' => false,
+            'format' => 'inline-menu',
+            'full_name' => false,
+            'order' => 'ASC',
+            'orderby' => 'code'
+        );
+    if ( is_string($args) ) {
+        $input = array('classes' => $args);
+    } else {
+        if ( is_array($args) ) {
+            $input = $args;
+        } else {
+            $input = array();
+        }
+    }
+    $params = array_replace_recursive($defaults, $input);
+
+
+    $languages = icl_get_languages('skip_missing=0&orderby='.$params['orderby'].'&order='.$params['order']);
     if(!empty($languages)){
-        echo '<ul id="lang_selector" class="'.$classes.'">';
+        if ($params['format'] === 'dropdown-button') {
+            echo '<button id="lang_selector" href="#" data-dropdown="languages" aria-controls="languages" aria-expanded="false" class="button dropdown '.$params['classes'].' ">'.ICL_LANGUAGE_NAME.'</button>';
+            echo '<ul class="f-dropdown" data-dropdown-content id="languages" aria-hidden="true">';
+        } else {echo '<ul id="lang_selector" class="'.$params['classes'].'">'; 
+        }   
+        if ($params['format'] === 'dropdown-menu') {
+            echo '<li class="has-dropdown"><a href="#">'.ICL_LANGUAGE_NAME.'</a>';
+            echo '<ul class="dropdown">';
+        } 
+        $i = 0;
         foreach($languages as $l){
+            if ($l['language_code'] == 'zh-hans') { $l['language_code'] = '中文'; }
             if ($l['active']) { $class = 'active'; } else {$class = '';}
+
+            if ( $params['has_dividers'] && $i > 0) echo "<li class='divider'></li>";
             echo '<li class="'.$class.'">';
             echo '<a href="'.$l['url'].'">';
-            echo ''.$l['language_code'].'';
+            $language_name = ($params['full_name'] ? $l['native_name'] : $l['language_code'] );
+            echo $language_name;
             echo '</a>';
             echo '</li>';
+            $i++;
         }
+        if ($params['format'] === 'dropdown-menu') echo '</ul></li>';
+        if ($params['format'] === 'dropdown-button') echo '</ul></button>';
     }
     echo '</ul>';
-}
-
-// Get language selector with just language code compatible with a foundation nav menu
-function mn_full_foundation_language_button( $classes = ''){
-    $languages = icl_get_languages('skip_missing=0&orderby=code');
-    if(!empty($languages)){
-        echo '<button href="#" data-dropdown="languages" aria-controls="languages" aria-expanded="false" class="button dropdown '.$classes.' ">'.ICL_LANGUAGE_NAME.'</button>';
-        echo '<ul class="f-dropdown" data-dropdown-content id="languages" aria-hidden="true">';
-        foreach($languages as $l){
-            if ( $l['active'] == '1') { $class = 'active'; } else {$class = '';}
-            echo '<li class="'.$class.'">';
-            echo '<a href="'.$l['url'].'">';
-            echo ''.$l['native_name'].'';
-            echo '</a>';
-            echo '</li>';
-        }
-        echo '</ul>'; // End dropdown
-    }
 }
 
 // Get language selector with just language code compatible with a foundation button
-function mn_full_foundation_language_nav( $classes = '' ){
-    $languages = icl_get_languages('skip_missing=0&orderby=code');
-    if(!empty($languages)){
-        echo '<button id="lang_selector" class="'.$classes.' ">';
-            echo '<li class="has-dropdown"><a href="#">'.ICL_LANGUAGE_NAME.'</a>';
-            echo '<ul class="dropdown">';
-            foreach($languages as $l){
-                if ( $l['active'] == '1') { $class = 'active'; } else {$class = '';}
-                echo '<li class="'.$class.'">';
-                echo '<a href="'.$l['url'].'">';
-                echo ''.$l['native_name'].'';
-                echo '</a>';
-                echo '</li>';
-            }
-            echo '</ul></li>'; // End dropdown
-    }
-    echo '</ul>';
+// Depreciated - use mn_language_selector instead
+function mn_full_foundation_language_button( $args = ''){
+    mn_language_selector( array('full_name' => true, 'format' => 'dropdown-button') );
+}
+
+// Get language selector with full name compatible with a foundation dropdown nav menu
+// Depreciated - use mn_language_selector instead
+function mn_full_foundation_language_nav( $args = '' ){
+    mn_language_selector( array('full_name' => true, 'format' => 'dropdown-menu') );
 }
 
 
@@ -137,5 +159,17 @@ function enqueue_utility_js() {
     wp_enqueue_script( 'mn_utility_js', plugin_dir_url( __FILE__ ) . '/javascript_utilities.js', array('jquery'), '1.0.0', true );
 }
 add_action('wp_enqueue_scripts','enqueue_utility_js');
+
+/**
+ * Add the_slug function
+ */
+function the_slug($echo=true){
+    $slug = basename(get_permalink());
+    do_action('before_slug', $slug);
+    $slug = apply_filters('slug_filter', $slug);
+    if( $echo ) echo $slug;
+    do_action('after_slug', $slug);
+    return $slug;
+}
 
 ?>
